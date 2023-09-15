a66a9415e5d65b196c2465e4fe580b85b2463de7
jbiccler <jaron.biccler@gmail.com>
2023-09-15 17:26:55 +0200

    feat: aliases for fish shell

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,3 +4,10 @@ end
 set -gx EDITOR nvim
 set fish_greeting
 starship init fish | source
+
+alias ls="lsd"
+alias gs="git status"
+alias ga="git add"
+alias gc="git commit"
+alias gp="git pull"
+alias gP="git push"
